8a8c13a699592ca78e4a6ad09abc8d7dddcd1417
modas <hy-cheng@outlook.com>
2022-12-05 16:43:27 +0800

    prompt 1

--- Chenghaoyang/friendship.py
+++ Chenghaoyang/friendship.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
+
 class UserFactory(metaclass=ABCMeta):
     """
     建立抽象用户工厂
@@ -7,36 +8,41 @@ class UserFactory(metaclass=ABCMeta):
     @abstractmethod
     def __init__(self):
         pass
-    
+
+
 class NormalUser(UserFactory):
     """
     普通用户类
     """
+
     def __init__(self, name, passwd):
         self.name = name
         self.passwd = passwd
         self.permission = 0
-        
-    
+
+
 class SuperUser(UserFactory):
     """
     管理员类
     """
+
     def __init__(self, name, passwd):
         self.name = name
         self.passwd = passwd
         self.permission = 1
-        
+
+
 class UserList():
     """
     用户类
     """
+
     def __init__(self):
         """
         建立用户名单
         """
         self.account_list = []
-    
+
     def make_user(self, cls, name, passwd):
         """建立新账号
 
@@ -49,10 +55,10 @@ class UserList():
             if account.name is name:
                 print('This account is already existed.\n')
                 return
-            
+
         self.account_list.append(eval(cls)(name, passwd))
         print('Successfully sign up.\n')
-        
+
     def list(self):
         """列出所有账户
 
@@ -60,7 +66,7 @@ class UserList():
             list: 所有账户列表
         """
         return self.account_list
-    
+
     def login(self, name, passwd):
         """登录
 
@@ -73,12 +79,13 @@ class UserList():
         """
         for account in self.account_list:
             if name is account.name and passwd is account.passwd:
-                print('Successfully sign in!\nYour name is {}.\nYou are a {}.\n'.format(account.name, 
+                print('Successfully sign in!\nYour name is {}.\nYou are a {}.\n'.format(account.name,
                       'Superuser' if account.permission == 1 else 'Normaluser'))
                 return account
-            
+
         print('There is no such account.\n')
-    
+
+
 class ContentFactory(metaclass=ABCMeta):
     """
     建立抽象内容工厂
@@ -86,32 +93,65 @@ class ContentFactory(metaclass=ABCMeta):
     @abstractmethod
     def __init__(self):
         pass
-    
+
+
 class NormaContent(ContentFactory):
     """
     一般内容
     """
-    def __init__(self):
-        print('this is a normal user')
-        
-    
+
+    def __init__(self, user, text):
+        """生成普通内容
+
+        Args:
+            user (用户类): 发表内容的用户
+            text (string): 内容文本
+        """
+        self.user = user
+        self.text = text
+
+
 class MediaContent(ContentFactory):
     """
     含附件内容
     """
-    def __init__(self):
-        print('this is a super user')
+
+    def __init__(self, user, text, annex):
+        """生成含附件内容
+
+        Args:
+            user (用户类): 发表内容的用户
+            text (string): 内容文本
+            annex (string): 模拟附件内容
+        """
+        self.user = user
+        self.text = text
+        self.annex = annex
         
-        
+
+
 class ContentList():
     def make_content():
         pass
-    
-    
+
+
+class prompt():
+    def __init__(self):
+        self.UserList = UserList()
+        print('Welcome! please sign in or sign up\n')
+        
+        c = input('Input 1 to sign up or input 2 to sign in\n>> ')
+        while c != '1' and c != '2':
+            c = input('Wrong input, please try again.\n>> ')
+            
+        if c is '1':
+            print('Sign up now.\n')
+            cls = input('Please input your permission\n>> ')
+            name = input('Please input your name\n>> ')
+            passwd = input('Please input your password\n>> ')
+            self.UserList.make_user(cls, name, passwd)  # 此处未作成功性检查
+            
+
+
 if __name__ == '__main__':
-    List = UserList()
-    List.make_user('SuperUser', 'modas', '123456')
-    List.make_user('SuperUser', 'modas', '123456')
-    acc = List.login('modas', '123456')
-
-
+    prompt()
